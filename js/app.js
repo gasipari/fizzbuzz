1e18b1e11772c7a048bdda9b34f5e4973d572f3b
Marius Muk <riamarius@gmail.com>
2015-07-07 21:48:39 -0400

    refactored FizzBuzz app

--- js/app.js
+++ js/app.js
@@ -6,17 +6,47 @@ $(document).ready(function(){
 		by both 3 and 5 with “fizz buzz”, 
 		those divisible by 3 with “fizz”, 
 		and those divisible by 5 with “buzz”.
-	*/
-	for (var i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 == 0) {
-			$("ul").append("<li>FizzBuzz</li>");
-			console.log('both...');
-		}else if (i % 3 == 0) {
-			$("ul").append("<li>Fizz</li>");
-		}else if (i % 5 == 0) {
-			$("ul").append("<li>Buzz</li>");
+		*/
+	// for (var i = 1; i <= 100; i++) {
+	// 	if (i % 3 == 0 && i % 5 == 0) {
+	// 		$("ul").append("<li>FizzBuzz</li>");
+	// 		console.log('both...');
+	// 	}else if (i % 3 == 0) {
+	// 		$("ul").append("<li>Fizz</li>");
+	// 	}else if (i % 5 == 0) {
+	// 		$("ul").append("<li>Buzz</li>");
+	// 	}else {
+	// 		$("ul").append("<li>"+i+"</li>");
+	// 	}
+	// }
+
+	// show button event listner
+	$("#showFizBtn").click(function() {
+		console.log('show Fizz');
+		var userNumber = $("#userNumber").val();
+		if (userNumber) {
+			calculateFizzBuzz(parseInt(userNumber));
 		}else {
-			$("ul").append("<li>"+i+"</li>");
+			alert("Please enter a number!");
+		}
+	})
+
+	/* calculate FizzBuzz
+	 * takes user number as input
+	 */
+	function calculateFizzBuzz(number) {
+		//console.log('user number is: '+typeof(number));
+		for (var i = 1; i <= number; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				$("ul").append("<li>FizzBuzz</li>");
+				console.log('both...');
+			}else if (i % 3 == 0) {
+				$("ul").append("<li>Fizz</li>");
+			}else if (i % 5 == 0) {
+				$("ul").append("<li>Buzz</li>");
+			}else {
+				$("ul").append("<li>"+i+"</li>");
+			}
 		}
 	}
 })
